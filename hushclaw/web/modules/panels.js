5c73ddca3ae91adfdadfefd9a09a67cf0284115f
tuanwei <tuanwei@tuanweideMacBook-Air.local>
2026-06-15 12:40:32 +0800

    Fix session paging panel re-export

--- hushclaw/web/modules/panels.js
+++ hushclaw/web/modules/panels.js
@@ -16,7 +16,7 @@ export {
   loadSession, renderSessions, renderSessionSearchResults,
   refreshSessionsView, runSessionSearch, scheduleSessionSearch, clearSessionSearch,
   toggleSessionsSidebar, initSessionsSidebarState,
-  updateSessionRunIndicator,
+  updateSessionRunIndicator, updateSessionPaging,
   onSessionDeleted, onSessionRenamed, handleSessionWorkspaceMoved,
   renderWorkspaceSelector,
   renderMemories, renderBeliefModels, renderBeliefModelsError, handleBeliefModelDetail,
